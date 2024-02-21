35d9fd6c221a072303c61d356855652b8a6724e4
Sreeja Prakash <prakashsreeja09@gmail.com>
2024-02-21 14:52:34 +0530

    customized

--- src/constants/index.js
+++ src/constants/index.js
@@ -122,6 +122,18 @@ export const experiences = [
         ],
     },
     {
+        title: "Trainee",
+        company_name: "Indira Gandhi Delhi Technical University for Women",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "May 2023 - June 2023",
+        points: [
+            "Learned Visual Computing with Pytorch and performed various activities and made projects in Deep Leanring using Pytorch",
+            "Learned about various neural networks and their implementations in Pytorch",
+           
+        ],
+    },
+    {
         title: "Technical Associate",
         company_name: "Minerva",
         icon: shopify,
@@ -201,6 +213,13 @@ export const socialLinks = [
 
 export const projects = [
     {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'NewsTop',
+        description: 'News application that calls the most recent news from the News API.Single page application with a navigation bar corresponding to different sections of the newspaper added functionality of infinite scroll along with a loader on page rendering.',
+        link: 'https://github.com/Sre-eja-09/NewsTop',
+    },
+    {
         iconUrl: pricewise,
         theme: 'btn-back-red',
         name: 'Amazon Price Tracker',
@@ -208,38 +227,17 @@ export const projects = [
         link: 'https://github.com/adrianhajdin/pricewise',
     },
     {
-        iconUrl: threads,
-        theme: 'btn-back-green',
-        name: 'Full Stack Threads Clone',
-        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-        link: 'https://github.com/adrianhajdin/threads',
-    },
-    {
-        iconUrl: car,
-        theme: 'btn-back-blue',
-        name: 'Car Finding App',
-        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
-    },
-    {
         iconUrl: snapgram,
         theme: 'btn-back-pink',
         name: 'Full Stack Instagram Clone',
-        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-        link: 'https://github.com/adrianhajdin/social_media_app',
-    },
-    {
-        iconUrl: estate,
-        theme: 'btn-back-black',
-        name: 'Real-Estate Application',
-        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-        link: 'https://github.com/adrianhajdin/projects_realestate',
+        description: 'A look alike of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/Sre-eja-09/snapgram_',
     },
     {
         iconUrl: summiz,
         theme: 'btn-back-yellow',
         name: 'AI Summarizer Application',
         description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+        link: 'https://github.com/Sre-eja-09/ai_summarizer',
     }
 ];
